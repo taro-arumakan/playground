2ab55fb28325f15b8e3e07aed71b716813306c78
taro <taro.arumakan@gmail.com>
2024-06-25 10:51:20 +0900

    image url csv generator - gbh cosmetics

---
+++ google_drive_link_images_download/image_url_importfile_generator.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+
+shopify_image_root = 'https://cdn.shopify.com/s/files/1/0655/9596/5639/files'
+out_products_csv_path = 'image_urls_gbh_cosmetics.csv'
+image_dir = '/Users/taro/sc/playground/images_gbh_cosmetics'
+df = pd.read_csv('/Users/taro/Downloads/products_export_gbh_cometics.csv')    # Shopify products export file
+
+
+
+df = df[['Handle', 'Title', 'Variant SKU']]
+df = df[df['Variant SKU'].notnull()]
+df['Variant SKU'] = df['Variant SKU'].apply(lambda x: x[1:] if x.startswith("'") else x)
+
+sku_images_map = {}
+for n in os.listdir(image_dir):
+  sku_images_map.setdefault(n.split('_')[1], []).append(n)
+
+expanded_rows = []
+
+for _, row in df.iterrows():
+    sku = row["Variant SKU"]
+    if sku in sku_images_map:
+        images = sku_images_map[sku]
+        images.sort(key=lambda x: (-2 if x.endswith('__a.jpg') else (
+                                   -1 if x.endswith('__b.jpg') else 
+                                   int(x.split('_')[-1].split('.')[0])), x))
+        position = 1
+        for img in images:
+            expanded_rows.append({
+                "Handle": row["Handle"],
+                "Title": row["Title"],
+                "Image Src": f'{shopify_image_root}/{img}',
+                "Image Position": position
+            })
+            position += 1
+
+expanded_df = pd.DataFrame(expanded_rows)
+expanded_df.to_csv(out_products_csv_path, index=False)
