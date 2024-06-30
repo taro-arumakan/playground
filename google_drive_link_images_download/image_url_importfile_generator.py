606c8375822ce99037d30a200c7bbe878b34cae7
taro <taro.arumakan@gmail.com>
2024-07-01 04:08:18 +0900

    more logging

--- google_drive_link_images_download/image_url_importfile_generator.py
+++ google_drive_link_images_download/image_url_importfile_generator.py
@@ -2,9 +2,9 @@ import os
 import pandas as pd
 
 shopify_image_root = 'https://cdn.shopify.com/s/files/1/0655/9596/5639/files'
-out_products_csv_path = 'image_urls_gbh_home.csv'
-image_dir = '/Users/taro/sc/playground/images_gbh_home'
-df = pd.read_csv('/Users/taro/Downloads/products_export_gbh_home.csv')    # Shopify products export file
+out_products_csv_path = 'image_urls_gbh_apparel.csv'
+image_dir = '/Users/taro/sc/playground/images_gbh_apparel_2'
+df = pd.read_csv('/Users/taro/Downloads/products_export_gbh_apparel.csv')    # Shopify products export file
 
 
 df = df[['Handle', 'Title', 'Variant SKU']]
@@ -13,18 +13,21 @@ df['Variant SKU'] = df['Variant SKU'].apply(lambda x: x[1:] if x.startswith("'")
 
 sku_images_map = {}
 for n in os.listdir(image_dir):
-  sku_images_map.setdefault(n.split('_')[1], []).append(n)
+  sku_images_map.setdefault(n.split('_')[2], []).append(n)
 
 expanded_rows = []
 
 title = None
 for _, row in df.iterrows():
     sku = row["Variant SKU"]
+    print(f'processing {sku}')
     if pd.notnull(row['Title']):
         position = 1
         title = row['Title']
+        print(f'processing {title}')
     if sku in sku_images_map:
         images = sku_images_map[sku]
+        print(f'processing {images}')
         images.sort(key=lambda x: (-2 if x.endswith('__a.jpg') else (
                                    -1 if x.endswith('__b.jpg') else 
                                    int(x.split('_')[-1].split('.')[0])), x))
