81789e05e3d70e59116fe95bd8ee5b9a781567e5
taro <taro.arumakan@gmail.com>
2024-07-09 16:21:39 +0900

    variant images

---
+++ variant_image_importfile_generator.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+outfile = '/Users/tao/Downloads/variant_images.csv'
+df = pd.read_csv('/Users/taro/Downloads/products_export 3.csv')
+images = df[['Image Src']]
+df = df[['Handle', 'Title', 'Option1 Name', 'Option1 Value', 'Option2 Name', 'Option2 Value', 'Variant SKU', 'Variant Image']]
+df = df[df['Variant SKU'].notnull()]
+df['Variant SKU'] = df['Variant SKU'].apply(lambda x: x.replace("'", ''))
+
+def image_url(sku):
+    res = images[images['Image Src'].str.contains(sku)]
+    if not res.empty:
+        return res.iloc[0]['Image Src']
+
+df['Variant Image'] = df['Variant SKU'].apply(image_url)
+df = df.ffill()
+df.to_csv(outfile)
